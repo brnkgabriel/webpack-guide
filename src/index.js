a0273b9fa3f94009be4b42d1ea1c48adb27d9734
brnkgabriel <brnkgabriel@gmail.com>
2018-10-15 16:41:35 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import _ from 'lodash';
+// import './style.css';
+// import BoardGames from './board-games.jpg';
+// import Data from './data.xml';
+
+// function component() {
+//   let element = document.createElement('div');
+
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.classList.add('hello');
+
+//   // Add the image to our existing div.
+//   var myBoardGames = new Image();
+//   myBoardGames.src = BoardGames;
+
+//   element.appendChild(myBoardGames);
+
+//   console.log(Data);
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
---
+++ test/sample-test.js
@@ -0,0 +1,5 @@
+describe('sample tests', function () {
+  it('should pass this test', function () {
+    expect(true).to.be.true;
+  });
+});
